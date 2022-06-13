f0a2db6656865163971f714c47cb32f9373e455a
Luke Gandolfi <lukegandolfi@icloud.com>
2022-06-13 16:46:04 +0100

    streamlit update

--- my_app/pages/prediction_1.py
+++ my_app/pages/prediction_1.py
@@ -4,54 +4,52 @@ from PIL import Image
 import os
 import streamlit as st
 
-st.write(os.getcwd())
-
-model = tf.keras.models.load_model('../static/model5.h5')
+# command = streamlit run my_app/streamlit_app.py from the root of the directory
+model = tf.keras.models.load_model('static/model5.h5')
 
 uploaded_file = st.file_uploader("Upload Image", 'gif')
 
-#image = Image
-# if uploaded_file:
+if uploaded_file:
 
-#     image = Image.open(uploaded_file) #.convert('RGB')
-#     #image_array  = tf.keras.preprocessing.image.img_to_array(image)
-#     image = tf.image.resize(image, (51,51,1))
-#     image = image / 255
-#     # image = tf.expand_dims(image, axis = 0)
+    image = Image.open(uploaded_file) #.convert('RGB')
+    image_array  = tf.keras.preprocessing.image.img_to_array(image)
+    image = tf.image.resize(image_array, (51, 51))
+    image = image / 255
+    image = tf.expand_dims(image, axis = 0)
+    result = model.predict(image)
 
-#     result = model.predict(image)
-
-#     st.write(result)
+    st.markdown(result)
 
 
-def predictor(img_path):
+# def predictor(img_path):
 
-    img = tf.keras.preprocessing.image.load_img(img_path, target_size=(51,51,1), color_mode='grayscale')
+#     img = tf.keras.preprocessing.image.load_img(img_path, target_size=(51,51,1), color_mode='grayscale')
 
-    img = tf.keras.preprocessing.image.img_to_array(img)
+#     img = tf.keras.preprocessing.image.img_to_array(img)
 
-    img = np.expand_dims(img,axis = 0)
+#     img = np.expand_dims(img,axis = 0)
 
-    prediction = model.predict(img)
+#     prediction = model.predict(img)
 
-    return prediction[0][0]
+#     return prediction[0][0]
 
-if uploaded_file is not None:
+# if uploaded_file is not None:
 
-    #if save_uploaded_file(uploaded_file):
+#     #if save_uploaded_file(uploaded_file):
 
-    # display the image
+#     # display the image
 
-    display_image = Image.open(uploaded_file)
+#     display_image = Image.open(uploaded_file)
 
-    st.image(display_image)
+#     st.image(display_image)
 
-    prediction = predictor(os.path.join('../static/images',uploaded_file.name))
+#     prediction = predictor(os.path.join('../static/images',uploaded_file.name))
 
-    os.remove('../static/images/'+uploaded_file.name)
 
-    # deleting uploaded saved picture after prediction
+#     os.remove('../static/images/'+uploaded_file.name)
 
-    # drawing graphs
+#     # deleting uploaded saved picture after prediction
 
-    st.text(f'Prediction: {prediction}')
+#     # drawing graphs
+
+#     st.text(f'Prediction: {prediction}')
